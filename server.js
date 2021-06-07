9a55bb57084db01c4089ca9aed5dd2169100f34c
Dianaiminza <dianaiminza.99@gmail.com>
2021-06-07 22:50:00 +0300

    my commit message

--- server.js
+++ server.js
@@ -41,12 +41,18 @@ app.get('/api/config/paypal', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 if(process.env.NODE_ENV ==='production'){
-  app.use(express.static( __dirname, 'build'));
+  // app.use(express.static('frontend/build'));
   
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'));
+  // app.get('*',(req,res)=>{
+  //   res.sendFile(path.resolve(__dirname,'build','index.html'));
   
-  });
+  // });
+
+  app.use(express.static(path.join(__dirname, '//frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}//frontend/build/index.html`));
+});
+
   }
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
